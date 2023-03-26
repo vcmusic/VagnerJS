806b230f5e1f8e3a854d44fce0647a609940605d
Vagner Campezatto <80751341+vcmusic@users.noreply.github.com>
2023-03-26 18:01:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+function testjs() {
+        
+
+    if (document.getElementById("Help").innerHTML=="Good morning!")
+    
+     {
+        document.getElementById("button").innerHTML= "CLICK HERE!";
+        document.getElementById("Help").innerHTML= "Good night!";
+        document.getElementById("day").innerHTML= "<span style='color: black;'>Have a great night!</span>";
+        
+    }
+    else {
+        document.getElementById("Help").innerHTML= "Good morning!"; 
+        document.getElementById("button").innerHTML= "CLICK HERE.. AGAIN!";
+        document.getElementById("day").innerHTML= "<span style='color: orange;'>Have a great day!</span>";
+    } 
+
+    }
+
+function calculate() {
+    const number1 = parseFloat(document.getElementById("number1").value);
+    const operator = document.getElementById("operator").value;
+    const number2 = parseFloat(document.getElementById("number2").value);
+    let result 
+  
+  
+    switch(operator) {
+      case "sum":
+        result = number1 + number2;
+        break;
+      case "subtraction":
+        result = number1 - number2;
+        break;
+      case "multiplication":
+        result = number1 * number2;
+        break;
+      case "division":
+        result = number1 / number2;
+        break;
+    }
+  
+    document.getElementById("result").value = result;
+  }
+
+function clearResult() {
+    const result = document.getElementById("result");
+    result.value = "";
+  }
